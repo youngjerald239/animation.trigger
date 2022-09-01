ef989cbf9845ee213246f86388b92708820e26b4
youngjerald239 <youngjerald239@gmail.com>
2022-09-01 12:47:04 -0500

    added ctx rotate

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ class Explosion {
         this.spriteHeight = 179
         this.width = this.spriteWidth * 0.7
         this.height = this.spriteHeight * 0.7
-        this.x = x - this.width/2
-        this.y = y - this.height/2
+        this.x = x 
+        this.y = y 
         this.image = new Image()
         this.image.src = './assets/boom.png'
         this.frame = 0
         this.timer = 0
+        this.angle = Math.random() * 6.2
     }
     update(){
         this.timer++
@@ -27,21 +28,36 @@ class Explosion {
         
     }
     draw(){
-        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
+        ctx.save()
+        ctx.translate(this.x, this.y)
+        ctx.rotate(this.angle)
+        ctx.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width/2, 0 - this.height/2, this.width, this.height)
+        ctx.restore()
     }
 }
 
 window.addEventListener('click', function(e){
+    createAnimation(e)
+})
+window.addEventListener('mousemove', function(e){
+    createAnimation(e)
+})
+
+function createAnimation(e){
     let positionX = e.x - canvasPosition.left
     let positionY = e.y - canvasPosition.top
     explosions.push(new Explosion(positionX, positionY))
-})
+}
 
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     for (let i = 0; i < explosions.length; i++) {
         explosions[i].update()
         explosions[i].draw()
+        if (explosions[i].frame > 5){
+            explosions.splice(i, 1)
+            i--
+        }
     }
     requestAnimationFrame(animate)
 }
